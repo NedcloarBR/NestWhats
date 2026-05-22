af754a9aeb87a296e84e4b88c972984d1e9b67a2
NedcloarBR <nedcloar1@hotmail.com>
2026-05-22 20:25:22 -0300

    chore: bump whatsapp-web.js to v1.34.7 and update ClientEvents

--- src/listeners/listener.interface.ts
+++ src/listeners/listener.interface.ts
@@ -1,17 +1,25 @@
 import {
 	BatteryInfo,
 	Call,
+	Chat,
 	GroupNotification,
 	Message,
 	MessageAck,
+	PollVote,
+	Reaction,
 	WAState,
 } from "whatsapp-web.js";
 
 export interface ClientEvents {
 	auth_failure: [message: string];
 	authenticated;
+	/** @deprecated Battery status events are no longer supported by WhatsApp */
 	change_battery: [batteryInfo: BatteryInfo];
+	change_state: [state: WAState];
 	call: [call: Call];
+	chat_archived: [chat: Chat, currState: boolean, prevState: boolean];
+	chat_removed: [chat: Chat];
+	code: [code: string];
 	contact_changed: [
 		message: Message,
 		oldId: string,
@@ -24,18 +32,22 @@ export interface ClientEvents {
 	group_leave: [notification: GroupNotification];
 	group_membership_request: [notification: GroupNotification];
 	group_update: [notification: GroupNotification];
-	loading_screen;
+	loading_screen: [percent: string, message: string];
 	media_uploaded: [message: Message];
 	message_ack: [message: Message, ack: MessageAck];
 	message_ciphertext: [message: Message];
+	message_ciphertext_failed: [message: Message];
 	message_create: [message: Message];
 	message_edit: [message: Message, newBody: string, prevBody: string];
 	message: [message: Message];
-	message_revoke_everyone: [message: Message, revoked_msg: Message];
+	message_reaction: [reaction: Reaction];
+	message_revoke_everyone: [message: Message, revoked_msg: Message | null];
 	message_revoke_me: [message: Message];
 	qr: [qr: string];
 	ready;
 	remote_session_saved;
+	unread_count: [chat: Chat];
+	vote_update: [vote: PollVote];
 }
 
 export type NestWhatsEvents = ClientEvents;
